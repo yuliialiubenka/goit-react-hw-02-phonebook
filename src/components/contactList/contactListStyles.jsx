168d05edc8a038d50fb55a9f1ebe94c0e4b47844
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-09-23 17:08:21 +0300

    Phonebook 2 React.JS

--- src/components/contactList/contactListStyles.jsx
+++ src/components/contactList/contactListStyles.jsx
@@ -15,7 +15,7 @@ export const useStyles = createUseStyles({
         marginRight: "-10px",
         "@media (max-width: 480px)": {
             marginRight: "-2px",
-            height: "calc(100vh - 340px)"
+            height: "calc(100% - 340px)"
         }
     },
     contactListItem: {
