4b3e2d314f4997125342078dc16a269c69e92ffe
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-09-25 18:21:36 +0300

    Update contactForm.jsx

--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -53,7 +53,7 @@ class ContactForm extends React.Component {
                         value={this.state.name}
                         onChange={this.handleChange}
                         placeholder='Name'
-                        minLength={2}
+                        pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                     />
@@ -72,7 +72,7 @@ class ContactForm extends React.Component {
                         value={this.state.number}
                         onChange={this.handleChange}
                         placeholder='Number'
-                        pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+                        pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                     />
